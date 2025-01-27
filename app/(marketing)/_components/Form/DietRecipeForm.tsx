98eb0cbee8533b506e08c2c4eb10b45e4d2ed9fd
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-01-27 17:25:31 +0900

    ダイエットモードの追加

---
+++ app/(marketing)/_components/Form/DietRecipeForm.tsx
@@ -0,0 +1,183 @@
+import { useForm } from 'react-hook-form'
+
+import { Recipe } from '@/domain/Recipe'
+import { RecipeRepository } from '@/infrastracture/repository/recipe_repository'
+
+import GenerateButton from './Button/GenerateButton'
+
+type DietRecipeFormProps = {
+  setRecipes: (recipes: Recipe[]) => void
+  setLoading: (loading: boolean) => void
+}
+
+type DietRecipeFormType = {
+  calorie: number
+  protein: number
+  fat: number
+  carbohydrate: number
+  salt: number
+  serves: number
+}
+
+const DietRecipeForm = ({ setRecipes, setLoading }: DietRecipeFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<DietRecipeFormType>({
+    defaultValues: {
+      calorie: 0,
+      protein: 0,
+      fat: 0,
+      carbohydrate: 0,
+      salt: 0,
+    },
+  })
+
+  const onSubmit = handleSubmit(async (data: DietRecipeFormType) => {
+    setLoading(true)
+    const repository = new RecipeRepository()
+
+    const response: Recipe[] = await repository.generateRecipeByDiet({
+      diets: {
+        calorie: data.calorie,
+        protein: data.protein,
+        fat: data.fat,
+        carbohydrate: data.carbohydrate,
+        salt: data.salt,
+      },
+      serves: data.serves,
+    })
+
+    setRecipes(response)
+    reset()
+    setLoading(false)
+  })
+  return (
+    <form
+      onSubmit={onSubmit}
+      className='w-full flex flex-col items-center gap-4'
+    >
+      <section className='w-full flex flex-col gap-4'>
+        <div className='w-full grid grid-cols-2 gap-4'>
+          <section className='space-y-2 w-full'>
+            <label htmlFor='calorie' className='text-sm'>
+              カロリー
+            </label>
+            <input
+              {...register('calorie', {
+                required: 'カロリーを入力してください',
+                min: {
+                  value: 0,
+                  message: '0以上の値を入力してください',
+                },
+              })}
+              min={0}
+              type='number'
+              className='w-full h-10 border-2 border-gray-300 rounded-md p-2'
+            />
+            {errors.calorie && (
+              <span className='text-red-500 text-xs'>
+                {errors.calorie.message}
+              </span>
+            )}
+          </section>
+          <section className='space-y-2 w-full'>
+            <label htmlFor='salt' className='text-sm'>
+              塩分
+            </label>
+            <input
+              {...register('salt', {
+                required: '塩分を入力してください',
+                min: {
+                  value: 0,
+                  message: '0以上の値を入力してください',
+                },
+              })}
+              min={0}
+              type='number'
+              className='w-full h-10 border-2 border-gray-300 rounded-md p-2'
+            />
+            {errors.salt && (
+              <span className='text-red-500 text-xs'>
+                {errors.salt.message}
+              </span>
+            )}
+          </section>
+        </div>
+        <div className='w-full grid grid-cols-3 gap-4'>
+          <section className='space-y-2 w-full'>
+            <label htmlFor='protein' className='text-sm'>
+              タンパク質
+            </label>
+            <input
+              {...register('protein', {
+                required: 'タンパク質を入力してください',
+                min: {
+                  value: 0,
+                  message: '0以上の値を入力してください',
+                },
+              })}
+              min={0}
+              type='number'
+              className='w-full h-10 border-2 border-gray-300 rounded-md p-2'
+            />
+            {errors.protein && (
+              <span className='text-red-500 text-xs'>
+                {errors.protein.message}
+              </span>
+            )}
+          </section>
+          <section className='space-y-2 w-full'>
+            <label htmlFor='fat' className='text-sm'>
+              脂質
+            </label>
+            <input
+              {...register('fat', {
+                required: '脂質を入力してください',
+                min: {
+                  value: 0,
+                  message: '0以上の値を入力してください',
+                },
+              })}
+              min={0}
+              type='number'
+              className='w-full h-10 border-2 border-gray-300 rounded-md p-2'
+            />
+            {errors.fat && (
+              <span className='text-red-500 text-xs'>{errors.fat.message}</span>
+            )}
+          </section>
+          <section className='space-y-2 w-full'>
+            <label htmlFor='carbohydrate' className='text-sm'>
+              炭水化物
+            </label>
+            <input
+              {...register('carbohydrate', {
+                required: '炭水化物を入力してください',
+                min: {
+                  value: 0,
+                  message: '0以上の値を入力してください',
+                },
+              })}
+              min={0}
+              type='number'
+              className='w-full h-10 border-2 border-gray-300 rounded-md p-2'
+            />
+            {errors.carbohydrate && (
+              <span className='text-red-500 text-xs'>
+                {errors.carbohydrate.message}
+              </span>
+            )}
+          </section>
+        </div>
+      </section>
+      <section>
+        <GenerateButton />
+      </section>
+    </form>
+  )
+}
+
+export default DietRecipeForm
