a2cbb9c2209f6f2760e327715ba30a87b763622f
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-01-30 16:14:38 +0900

    deployエラーの解消

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import './globals.css'
 
 import { Shippori_Mincho } from 'next/font/google'
 
-import Header from '@/components/Header'
-import { CurrentUserProvider } from '@/providers/CurrentUserProvider'
-
 const shippori_mincho = Shippori_Mincho({
   subsets: ['latin'],
   weight: '400',
