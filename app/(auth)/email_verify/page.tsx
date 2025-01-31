3d3e38efb34d4f478f212e5bbb287594d9c681a7
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-01-31 14:47:25 +0900

    ヘッダーコンポーネントのデザイン修正

--- app/(auth)/email_verify/page.tsx
+++ app/(auth)/email_verify/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation'
 import { CheckEmailVerificationUseCase } from '@/usecase/CheckEmailVerificationUseCase/CheckEmailVerificationUseCase'
 import { ResendEmailVerificationUseCase } from '@/usecase/ResendEmailVerificationUseCase/ResendEmailVerificationUseCase'
 
+import Header from '../_components/Header'
+
 const Page = () => {
   const router = useRouter()
 
@@ -34,30 +36,33 @@ const Page = () => {
   }
 
   return (
-    <div className='max-w-screen-sm mx-auto py-16 px-8'>
-      <div className='px-8 md:px-16 py-8 bg-gray-100 rounded-lg shadow-md'>
-        <div className='flex flex-col items-center gap-8'>
-          <div className='flex flex-col items-center gap-4'>
-            <h3>登録したメールに確認メールを送信しました</h3>
+    <div className='w-full min-h-screen'>
+      <Header />
+      <div className='max-w-screen-sm mx-auto py-16 px-8'>
+        <div className='px-8 md:px-16 py-8 bg-gray-100 rounded-lg shadow-md'>
+          <div className='flex flex-col items-center gap-8'>
+            <div className='flex flex-col items-center gap-4'>
+              <h3>登録したメールに確認メールを送信しました</h3>
 
-            <button
-              onClick={handleCheckEmail}
-              className='px-8 py-2 text-white font-semibold bg-green-500 rounded-full shadow-md
+              <button
+                onClick={handleCheckEmail}
+                className='px-8 py-2 text-white font-semibold bg-green-500 rounded-full shadow-md
                 hover:shadow-none hover:bg-green-600 hover:translate-y-1 duration-300'
-            >
-              確認しました
-            </button>
-          </div>
+              >
+                確認しました
+              </button>
+            </div>
 
-          <div className='flex flex-col items-center gap-4'>
-            <h3>確認メールを再度受け取りたい方はこちらから</h3>
-            <button
-              onClick={handleResendEmail}
-              className='px-8 py-2 text-white font-semibold bg-pink-500 rounded-full shadow-md 
+            <div className='flex flex-col items-center gap-4'>
+              <h3>確認メールを再度受け取りたい方はこちらから</h3>
+              <button
+                onClick={handleResendEmail}
+                className='px-8 py-2 text-white font-semibold bg-pink-500 rounded-full shadow-md 
                 hover:shadow-none hover:bg-pink-600 hover:translate-y-1 duration-300'
-            >
-              確認メールを再送信
-            </button>
+              >
+                確認メールを再送信
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className='w-full min-h-screen flex justify-center items-center gap-6 mt-10'>
-      <div className='h-10 w-10 animate-spin border-[5px] border-sky-400 rounded-full  border-t-transparent'></div>
-      <p className='text-[30px] font-weight'>Loading</p>
+    <div className='w-full min-h-screen flex justify-center items-center gap-6'>
+      <div className='h-14 w-14 animate-spin border-[6px] border-red-400 rounded-full border-t-transparent'></div>
+      <p className='text-4xl font-semibold animate-pulse'>Loading</p>
     </div>
   )
 }
